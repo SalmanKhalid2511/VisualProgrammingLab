9a44f882ab4300a17555c97ecb17fd607bbc36c8
SalmanKhalid2511 <233547@students.au.edu.pk>
2024-10-18 16:06:59 +0500

    Add files via upload

---
+++ Lab5Ex1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+namespace std
+{
+    class Person
+    {
+        public string name { get; set; }
+        public Person() { name = null; }
+        public Person(string name)
+        { this.name = name; }
+    }
+    class Student : Person
+    {
+        public string regNo { get; set; }
+        public int age { get; set; }    
+        public string program { get; set; }
+        public Student()
+        {
+            regNo = null;
+            age = 0;
+            program = null;
+        }
+        public Student(string regNo, int age, string program)
+        {
+            this.regNo = regNo;
+            this.age = age;
+            this.program = program;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+            student.name = "Salman";
+            student.regNo = "233547";
+            student.age = 22;
+            student.program = "BSCS";
+
+            Console.WriteLine($"Name : {student.name} ");
+            Console.WriteLine($"registraton number : {student.regNo} ");
+            Console.WriteLine($"Age : {student.age}" );
+            Console.WriteLine($"Program {student.program}: ");
+
+        }
+    }   
+}
---
+++ Lab5Ex2.cs
@@ -0,0 +1,57 @@
+﻿using System;
+namespace std
+{
+    class Customer
+    {
+        public int customerId { set; get; }
+        public string firstName { set; get; }
+        public string lastName { set; get; }
+        public string street { set; get; }
+        public string city { set; get; }
+        public string state { set; get; }
+        public int Zipcode { set; get; }
+        public string phone { set; get; }
+        public string email { set; get; }
+        public string password { set; get; }
+    }
+    class RetialCustomer
+    {
+        public string CreditCardType { set; get; }
+        public string CreditCardNo { set; get; }
+
+    }
+    class CorporateCustomer
+    {
+        public string CompanyName { set; get; }
+        public int freguentFlyerPts { set; get; }
+        public string billingAccountNo { set; get; }
+    }
+    class Reservation
+    {
+        public int reservationNo { set; get; }
+        public DateTime date { set; get; }
+        public Customer customer { set; get; }
+        public List<Seat> seats { set; get; }
+    }
+    class Flight
+    {
+        public int flightId { set; get; }
+        public DateTime date { set; get; }
+        public string origin { set; get; }
+        public string destination { set; get; }
+        public DateTime departureTime { set; get; }
+        public DateTime arrivalTime { set; get; }
+        public int seatingCapacity { set; get; }
+        public List<Seat> seats { set; get; }
+    }
+    class Seat
+    {
+        public int seatNo { set; get; }
+        public int rowNo { set; get; }
+        public decimal price { set; get; }
+        public string status { set; get; }
+        public Flight flight { set; get; }
+        public Reservation reservation { set; get; }
+    }
+}
+ 
