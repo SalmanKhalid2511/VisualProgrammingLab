a96bd2658f18f9652e56b939e623984bce398d02
SalmanKhalid2511 <233547@students.au.edu.pk>
2025-01-10 23:18:13 +0500

    Add files via upload

---
+++ Assignment5/Assigmnet 5/Components/Data/CityRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace CityButtonApp.Data
+{
+    public static class CityRepository
+    {
+        public static List<string> GetCities()
+        {
+            return new List<string> { "New York", "London", "Tokyo", "Paris", "Sydney" };
+        }
+    }
+}
